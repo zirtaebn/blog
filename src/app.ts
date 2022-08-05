0b86763b3baa3612cd2b0612eb5f39a537cfbd4e
Beatriz Sant'Anna <zirtaebn@gmail.com>
2022-08-04 23:25:57 -0300

    add mustache set up

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
+import path from 'path';
+
+import mustache from 'mustache-express';
+
 dotenv.config();
 
 const app = express();
 
+app.set('view engine', 'mustache');
+app.set('views', path.join(__dirname, 'views'));
+app.engine('mustache', mustache());
+
 app.get('/', (req, res) => {
 
-    res.send('Hello World!')
+    res.render('./pages/home')
 })
 
 app.listen(process.env.PORT, () => console.log('Server up and running.'));
